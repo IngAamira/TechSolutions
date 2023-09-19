01b461fa1b63a05b4700b031df2c9d9b92e762e6
aamiramm <67479844+IngAamira@users.noreply.github.com>
2023-09-19 09:41:21 -0500

    Initial commit

---
+++ src/main/java/com/ingaamira/techsolutions/TechSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.ingaamira.techsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingaamira/techsolutions/TechSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingaamira.techsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
